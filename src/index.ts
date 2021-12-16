bb99aaf9e4869996a132344641d802ff02322e7e
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2021-12-16 18:23:19 -0300

    docs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
+import swaggerFile from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
